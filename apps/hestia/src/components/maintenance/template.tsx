e9462f7002949838b1c393ea72fdf1109f1b8836
Kas <kas_pintxuki@outlook.com>
2024-12-16 13:08:23 -0300

    fix: maintenance page text

--- apps/hestia/src/components/maintenance/template.tsx
+++ apps/hestia/src/components/maintenance/template.tsx
@@ -19,23 +19,20 @@ export function Template() {
           </Link>
         </div>
         <div className="flex-1 h-full flex max-md:flex-col bg-level-0 rounded-lg items-center md:px-4">
-          <div className="flex flex-col gap-5 max-md:p-3 md:p-12 max-w-[500px]">
+          <div className="flex flex-col gap-4 max-md:p-3 md:p-12 max-w-[500px]">
             <Typography.Heading size="5xl">
               We&apos;re improving your experience
             </Typography.Heading>
-            <Typography.Heading size="xl">
-              ⚠️ Heads up traders ⚠️
-            </Typography.Heading>
-            <Typography.Text size="md">
-              Polkadex Orderbook will be temporarily unavailable for a scheduled
-              downtime of 6 to 12 hours starting Wednesday May 22nd at 9am UTC.
+            <Typography.Text size="xl">
+              Polkadex Orderbook is temporarily down to improve your experience.
+              Hyperbridging and other UX changes are underway.
             </Typography.Text>
-            <Typography.Text size="md">
-              During this time, LMP bug fixes and direct deposits & withdrawal
-              functionality will be deployed to the backend 🛠️
-            </Typography.Text>
-            <div className="flex flex-col gap-2">
-              <Typography.Text appearance="primary">Need help?</Typography.Text>
+
+            <div className="flex flex-col gap-4">
+              <Typography.Text appearance="primary">
+                If you’re experiencing any issues after withdrawing your tokens
+                from Polkadex Orderbook or Thea, please contact us.
+              </Typography.Text>
               <div className="flex items-center gap-2 flex-wrap">
                 <Button.Icon
                   asChild
