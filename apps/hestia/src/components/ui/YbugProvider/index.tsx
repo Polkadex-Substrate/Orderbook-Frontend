b8a05167f2d9fab562017a9eab6c648670bff464
nilesh-wow <nilesh.dangi@wowlabz.com>
2026-05-26 13:38:50 +0530

    feat: enhance YbugProvider with settings for launcher position and translation

--- apps/hestia/src/components/ui/YbugProvider/index.tsx
+++ apps/hestia/src/components/ui/YbugProvider/index.tsx
@@ -7,5 +7,15 @@ const ybugId = process.env.NEXT_PUBLIC_YBUG_ID;
 
 export const YbugProvider = ({ children }: { children: ReactNode }) => {
   if (!ybugId) return <>{children}</>;
-  return <YbugProviderLib ybugId={ybugId}>{children}</YbugProviderLib>;
+  return (
+    <YbugProviderLib
+      ybugId={ybugId}
+      settings={{
+        launcher_position: "top-middle",
+        translate: { "launcherButton.Title": "Report an Issue" },
+      }}
+    >
+      {children}
+    </YbugProviderLib>
+  );
 };
